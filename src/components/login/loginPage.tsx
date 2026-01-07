f7d488b5d6556b460e88d2b7cd1ca4c35b1f1411
hodarasaei83 <hodarasaei83@gmail.com>
2026-01-07 11:20:30 +0330

    Add/login page & Update/folder structure

--- src/components/login/loginPage.tsx
+++ src/components/login/loginPage.tsx
@@ -1,5 +1,27 @@
-export default function LoginPage(){
+import { Card, Layout } from 'antd'
+
+export default function LoginPage() {
   return (
-    <div>loginPage</div>
+    <Layout
+      className="login-gradient"
+      style={{
+        minHeight: '852px',
+        minWidth: '393px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 0,
+        margin: 0,
+      }}
+    >
+      <Card
+        style={{
+          background: 'none',
+          border: 'none',
+        }}
+      >
+        <img src="/loginLogo.svg" alt="logo" />
+      </Card>
+    </Layout>
   )
 }
