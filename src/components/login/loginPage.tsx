3589c69f3dbff918d61cb00f0c98d94d5c14d23e
hodarasaei83 <hodarasaei83@gmail.com>
2026-01-07 15:13:25 +0330

    Add/step one of login page ui

--- src/components/login/loginPage.tsx
+++ src/components/login/loginPage.tsx
@@ -1,4 +1,7 @@
-import { Card, Layout } from 'antd'
+import { PhoneOutlined } from '@ant-design/icons'
+import { Button, Card, Form, Input, Layout, Typography } from 'antd'
+
+const { Title } = Typography
 
 export default function LoginPage() {
   return (
@@ -8,7 +11,8 @@ export default function LoginPage() {
         minHeight: '852px',
         minWidth: '393px',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         padding: 0,
         margin: 0,
@@ -18,10 +22,98 @@ export default function LoginPage() {
         style={{
           background: 'none',
           border: 'none',
+          marginTop: '40px',
         }}
       >
         <img src="/loginLogo.svg" alt="logo" />
       </Card>
+      <Form
+        variant="outlined"
+        style={{
+          backgroundColor: 'white',
+          width: '100%',
+          height: '573px',
+          borderRadius: '40px 40px 0px 0px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Card
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            border: 'none',
+            padding: 0,
+            margin: 0,
+            background: 'none',
+          }}
+        >
+          <Title level={4} style={{ textAlign: 'right' }}>
+            ورود
+          </Title>
+
+          <Title
+            type="secondary"
+            style={{
+              textAlign: 'right',
+              fontSize: '14px',
+              marginBottom: '30px',
+            }}
+          >
+            برای ادامه، شماره تلفن همراه خود را وارد کنید
+          </Title>
+
+          <Form.Item
+            rules={[{ required: true, message: 'این فیلد الزامی است' }]}
+            name="Input"
+          >
+            <Input
+              dir="rtl"
+              style={{
+                display: 'flex',
+                height: '56px',
+                gap: '10px',
+                fontSize: '15px',
+              }}
+              size="large"
+              placeholder="شماره تلفن همراه"
+              suffix={
+                <PhoneOutlined
+                  style={{
+                    transform: 'rotate(90deg)',
+                    color: '#9CA3AF',
+                    fontSize: '20px',
+                  }}
+                />
+              }
+            />
+          </Form.Item>
+        </Card>
+
+        <Card
+          style={{
+            background: 'none',
+            border: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Button
+            style={{
+              backgroundColor: '#4A6FA5',
+              color: 'white',
+              width: '400px',
+              height: '56px',
+              borderRadius: '8px',
+            }}
+          >
+            دریافت کد
+          </Button>
+        </Card>
+      </Form>
     </Layout>
   )
 }
